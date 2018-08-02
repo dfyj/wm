6b5925d59f5881bd6af7253b70ea65a3af9dbb23
CUFEWGS <41976116+CUFEWGS@users.noreply.github.com>
2018-08-02 16:32:02 +0800

    Revert "Revert "rename the risk and return analysis and add attribution analysis""
    This reverts commit 9c02902541c9ecdabf62be29c0d304454db918e7.

--- R/attr/致远产品/import.R
+++ R/attr/致远产品/import.R
@@ -11,7 +11,7 @@ products <- readxl::excel_sheets(nav_file_path)
 
 f_read_sheets <- function(prod){
   df <- read_excel(
-    path,
+    nav_file_path,
     sheet = prod,
     range = cell_cols(2:4),
     col_types = c("text", "numeric", "numeric")) %>%
@@ -29,7 +29,7 @@ nav_df_list <- products %>%
 
 nav_df <-
   nav_df_list %>%
-  bind_rows(, .id = "product")
+  bind_rows( , .id = "product")
 
 # diagnostics
 
@@ -47,65 +47,113 @@ nav_df <-
 Rb <- wsd_return_xts(bm, min(nav_df$DATETIME), max(nav_df$DATETIME))
 
 # calc portflio metrics ---------------------------------------------------
+f_get_weekly_ret <- function(df) {
+  df %>%
+    df_to_xts() %>%
+    xts::apply.weekly(xts::last) %>%
+    Return.calculate
+}
 
 f_calc_port_metrics <-  function(df, Rb) {
   ret.weekly <-
     df %>%
-    df_to_xts() %>%
-    apply.weekly(last) %>%
-    PerformanceAnalytics::Return.calculate()
+    f_get_weekly_ret
 
   bind_cols(
     summarise(df,
               起始日 = min(DATETIME),
               净值截止日 = max(DATETIME)),
-    metrics_weekly_ret(ret.weekly),
-    metrics_bm_based(ret.weekly, Rb)
+    metrics_risk_ret(ret.weekly),
+    metrics_risk_CAMP(ret.weekly, Rb),
+    metrics_risk_TM(ret.weekly, Rb)
   )
 }
 
 # df <- nav_df_list[[1]]
 # f_calc_port_metrics(df, Rb)
 
-result <- map_df(nav_df_list, f_calc_port_metrics, Rb, .id = "产品")
+risk_ret_result <- map_df(nav_df_list, f_calc_port_metrics, Rb, .id = "产品")
+
+
+# attribution based on alpha and beta------------------------------------------------------
+
+f_calc_alpha_beta <-  function(df, Rb) {
+  ret.weekly <-
+    df %>%
+    f_get_weekly_ret
+
+  alpha_beta_sig = attr_alpha_beta(ret.weekly,Rb)
+
+  bind_cols(
+    summarise(df,
+              起始日 = min(DATETIME),
+              净值截止日 = max(DATETIME)),
+    alpha = alpha_beta_sig[1, "mean"],
+    alpha显著性 = alpha_beta_sig[1, "sig"],
+    beta = alpha_beta_sig[2, "mean"],
+    beta显著性 = alpha_beta_sig[2, "sig"]
+  )
+}
+
+alpha_beta_result <- map_df(nav_df_list, f_calc_alpha_beta, Rb, .id = "产品")
+
+
+# attribution based on alpha_only -----------------------------------------
+
+f_calc_alpha_only <-  function(df, Rf = 0) {
+  ret.weekly <-
+    df %>%
+    f_get_weekly_ret
+
+  alpha_sig = attr_alpha(ret.weekly,Rf)
+
+  bind_cols(
+    summarise(df,
+              起始日 = min(DATETIME),
+              净值截止日 = max(DATETIME)),
+    alpha = alpha_sig["mean"],
+    alpha显著性 = alpha_sig["sig"]
+  )
+}
+
+alpha_only_result <- map_df(nav_df_list, f_calc_alpha_only, Rf = 0, .id = "产品")
+
 
 
 # export data -------------------------------------------------------------
 
 .dir <- dirname(nav_file_path)
 
-result %>%
+risk_ret_result %>%
   write.csv(
-    file.path(.dir, "致远组合分析.csv"),
+    file.path(.dir, "致远组合风险收益分析.csv"),
     fileEncoding = "gbk", row.names = FALSE)
 
-result %>%
-  rds_write(file.path(.dir,"致远组合分析.rds"))
+alpha_beta_result %>%
+  write.csv(
+    file.path(.dir, "致远组合alpha_beta分析.csv"),
+    fileEncoding = "gbk", row.names = FALSE)
+
+alpha_only_result %>%
+  write.csv(
+    file.path(.dir, "致远组合alpha_only分析.csv"),
+    fileEncoding = "gbk", row.names = FALSE)
+
+risk_ret_result %>%
+  rds_write(
+    file.path(.dir, "致远组合alpha_only分析.rds"))
+
+alpha_beta_result %>%
+  rds_write(
+    file.path(.dir, "致远组合alpha_only分析.rds"))
+
+alpha_only_result %>%
+  rds_write(
+    file.path(.dir, "致远组合alpha_only分析.rds"))
+
 
 nav_df %>%
   rds_write(
     file.path(.dir, "NAV.rds"))
 
-
-# attribution -------------------------------------------------------------
-
-f_get_weekly_ret <- function(df) {
-  df %>%
-    df_to_xts() %>%
-    xts::apply.weekly(xts::last) %>%
-    PerformanceAnalytics::Return.calculate
-}
-
-list_ret.week <-
-  nav_df_list[
-    #   c(
-    #   "致远进取一号",
-    #   "致远稳健一号"
-    # )
-    ] %>%
-  map(f_get_weekly_ret)
-
-list_ret.week %>%
-  # map_df(attr_alpha, .id = "product")
-  map_df(attr_alpha_beta, Rb, .id = "product")
 
