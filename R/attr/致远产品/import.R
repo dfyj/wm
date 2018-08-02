6dc57f70d9c4ce1f026409096306fd3c5afae90a
xk152004 <xk152004@gmail.com>
2018-08-02 13:37:35 +0800

    clean up
    clean up

--- R/attr/致远产品/import.R
+++ R/attr/致远产品/import.R
@@ -1,97 +1,111 @@
 library(readxl)
 
 
-path <- "C:/OSC/wm/R/attr/致远产品/致远产品日净值表20180727（仅供内部参考）.xlsx"
+# import ------------------------------------------------------------------
+
+nav_file_path <- "./attr/致远产品/致远产品日净值表20180727（仅供内部参考）.xlsx"
 
 bm <- "000905.SH"
 
-products <- readxl::excel_sheets(path)
-#products %>% filter(products)
-# prod <- products[1]
-# f_import_nav <- function(prod){
-#   readxl::read_excel(path, sheet = prod) %>%
-#     setNames(c("NAME","DATETIME", "unit_nav","cum_nav")) %>%
-#     mutate(DATETIME = as.character(DATETIME))
-# }
-# if(nrow(filter(df,!is.na(x)))<= 0)
+products <- readxl::excel_sheets(nav_file_path)
 
-products <- readxl::excel_sheets(path)
+f_read_sheets <- function(prod){
+  df <- read_excel(
+    path,
+    sheet = prod,
+    range = cell_cols(2:4),
+    col_types = c("text", "numeric", "numeric")) %>%
+    setNames(c("DATETIME", "UNIT_NAV", "CUM_NAV")) %>%
+    mutate(DATETIME = as.character(lubridate::parse_date_time(DATETIME, "%y%m%d")))
 
-read_sheets <- function(prod){
-  df <- read_excel(path,sheet = prod,range = cell_cols(2:4),
-                   col_types = c("guess", "numeric","numeric"))%>%
-    setNames(c("DATETIME", "UNIT_NAV","CUM_NAV")) %>%
-    mutate(DATETIME = as.character(DATETIME)) %>%
-    mutate(DATETIME = as.character((lubridate::parse_date_time(DATETIME,"%y%m%d"))))
-  if(nrow(filter(df,!is.na(CUM_NAV))) <= 0)
+  if(nrow(filter(df, !is.na(CUM_NAV))) <= 0)
     df$CUM_NAV <- df$UNIT_NAV
   df <- df[,c(1,3)]
 }
 
-df_list <- products %>%
-  map(read_sheets) %>%
+nav_df_list <- products %>%
+  map(f_read_sheets) %>%
   set_names(products)
 
+nav_df <-
+  nav_df_list %>%
+  bind_rows(, .id = "product")
 
-df <- products %>%
-  map_df(read_sheets)
-# as.date(date_range$start)
-# lubridate::parse_date_time("20180101", "%y%m%d") %>% as.character()
+# diagnostics
 
-dt_range <- function(df){
-  df %>%
-    mutate(start = min(DATETIME), end = max(DATETIME))
-}
+# df %>%
+#   group_by(product) %>%
+#   summarise(
+#     min(DATETIME),
+#     max(DATETIME),
+#     min(CUM_NAV),
+#     max(CUM_NAV),
+#     n()
+#   ) %>%
+#   View()
 
-date_range <- df_list %>%
-  map_df(dt_range, .id = "product") %>%
-  summarise(start = min(start), end = max(end))
+Rb <- wsd_return_xts(bm, min(nav_df$DATETIME), max(nav_df$DATETIME))
 
-Rb <- wsd_return_xts(bm,date_range$start,date_range$end)
+# calc portflio metrics ---------------------------------------------------
 
-
-# ret <- df_list[[prod]] %>%
-#   df_to_xts()%>%
-#   apply.weekly(last) %>%
-#   PerformanceAnalytics::Return.calculate()%>%
-#   metrics_weekly_ret() %>%
-#   metrics_bm_based(.,Rb)
-
-#analysize the portflios
-f <-  function(df, Rb){
-  ret <-
-  df %>%
+f_calc_port_metrics <-  function(df, Rb) {
+  ret.weekly <-
+    df %>%
     df_to_xts() %>%
     apply.weekly(last) %>%
     PerformanceAnalytics::Return.calculate()
-  start =   (df %>%
-    summarise( 起始日 = min(DATETIME)))
-  end = (df %>%
-             summarise(净值截止日 = max(DATETIME)))
+
   bind_cols(
-    start,
-    end,
-    metrics_weekly_ret(ret),
-    metrics_bm_based(ret, Rb)
+    summarise(df,
+              起始日 = min(DATETIME),
+              净值截止日 = max(DATETIME)),
+    metrics_weekly_ret(ret.weekly),
+    metrics_bm_based(ret.weekly, Rb)
   )
-  }
+}
+
+# df <- nav_df_list[[1]]
+# f_calc_port_metrics(df, Rb)
+
+result <- map_df(nav_df_list, f_calc_port_metrics, Rb, .id = "产品")
 
 
-# f(df_list[["致远激进三号"],Rb)
-# df <- df_list[["致远激进三号"]]
-# %>%
-#   df_to_xts() %>%
-#   apply.weekly(last)
+# export data -------------------------------------------------------------
+
+.dir <- dirname(nav_file_path)
+
+result %>%
+  write.csv(
+    file.path(.dir, "致远组合分析.csv"),
+    fileEncoding = "gbk", row.names = FALSE)
+
+result %>%
+  rds_write(file.path(.dir,"致远组合分析.rds"))
+
+nav_df %>%
+  rds_write(
+    file.path(.dir, "NAV.rds"))
 
 
-# result<- map(df_list, safely(f), Rb)
-result<- map_df(df_list, f, Rb, .id = "产品")
-# result %>%
-  # transpose()
+# attribution -------------------------------------------------------------
 
-#export data
-path  <-  "C:/OSC/wm/R/attr/致远产品"
-write.csv(result,file = file.path(path,"致远组合分析.csv"),fileEncoding= "gbk",row.names = FALSE)
-rds_write(result,file.path(path,"致远组合分析.rds"))
-rds_write(df_list,file.path(path,"NAV.rds"))
+f_get_weekly_ret <- function(df) {
+  df %>%
+    df_to_xts() %>%
+    xts::apply.weekly(xts::last) %>%
+    PerformanceAnalytics::Return.calculate
+}
+
+list_ret.week <-
+  nav_df_list[
+    #   c(
+    #   "致远进取一号",
+    #   "致远稳健一号"
+    # )
+    ] %>%
+  map(f_get_weekly_ret)
+
+list_ret.week %>%
+  # map_df(attr_alpha, .id = "product")
+  map_df(attr_alpha_beta, Rb, .id = "product")
 
